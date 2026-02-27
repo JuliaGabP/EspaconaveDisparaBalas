bceaf1b54287208fb2cc2675f0a52415c53021d2
Julia <juliagabpaulino@outlook.com>
2026-02-27 14:56:39 -0300

    Adicionando segunda parte do desenvolvimento: Refatorando

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,38 +3,44 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
-	"""Classe geral para gerenciar ativos e comportamento do jogo"""
-	
-	def __init__(self):
-		"""Inicializa o jogo e crie recursos do jogo"""
-		pygame.init()
-		self.clock = pygame.time.Clock()
-		self.settings = Settings()
-		
-		self.screen = pygame.display.set_mode(
-		    (self.settings.screen_width, self.settings.screen_height))
-		pygame.display.set_caption("Alien Invasion")
-		
-		self.ship = Ship(self)
-		
-	def run_game(self):
-		"""Inicia o loop principal do jogo"""
-		while True:
-		    # Observa eventos de teclado e mouse
-		    for event in pygame.event.get():
-			    if event.type == pygame.QUIT:
-			    	sys.exit()
-			
-		    # Redesenha a tela durante cada passagem pelo loop
-		    self.screen.fill(self.settings.bg_color)
-		    self.ship.blitme()
-			
-		    # Deixa a tela desenhada mais recente visível
-		    pygame.display.flip()
-		    self.clock.tick(60)
-		
+    """Classe geral para gerenciar ativos e comportamento do jogo"""
+
+    def __init__(self):
+        """Inicializa o jogo e cria recursos do jogo"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Inicia o loop principal do jogo"""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """Responde às teclas pressionadas e a eventos de mouse"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Atualiza as imagens na tela e muda para a nova tela"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
 if __name__ == '__main__':
-	# Cria uma instância do jogo e execute o jogo
-	ai = AlienInvasion()
-	ai.run_game()
+    # Cria uma instância do jogo e executa o jogo
+    ai = AlienInvasion()
+    ai.run_game()
