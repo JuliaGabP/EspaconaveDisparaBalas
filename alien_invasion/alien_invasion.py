50551b7679a29c71670b522c9f9bd792759a5931
Julia <juliagabpaulino@outlook.com>
2026-03-02 12:47:39 -0300

    Adicionando quarta parte do desenvolvimento: Disparos

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 
 class AlienInvasion:
     """Classe geral para gerenciar ativos e comportamento do jogo"""
@@ -18,12 +18,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-
+        self.bullets = pygame.sprite.Group()
+        
     def run_game(self):
         """Inicia o loop principal do jogo"""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -45,6 +47,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
             
     def _check_keyup_events(self, event):
         """Responde teclas soltas"""
@@ -53,11 +57,28 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False    
                     
-                    
-                    
+    def _fire_bullet(self):
+        """Cria um novo projétil e o adiciona ao grupo de projéteis"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """Atualiza a posição dos projéteis e descarta os projéteis antigos"""
+        # Atualiza as posições dos projéteis
+        self.bullets.update()
+        
+        #Descarta os projéteis que desapareceram
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+                
+    
     def _update_screen(self):
         """Atualiza as imagens na tela e muda para a nova tela"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         pygame.display.flip()
 
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,3 +9,10 @@ class Settings:
 		
 		# Configurações da espaçonave
 		self.ship_speed = 1.5
+		
+		# Configurações do projétil
+		self.bullet_speed = 2.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
