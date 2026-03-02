54901592cc6330b64f9991ad88e90666b8436d2f
Julia <juliagabpaulino@outlook.com>
2026-03-02 15:06:09 -0300

    Adicionando sétima parte do desenvolvimento: Movendo a frota

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -30,6 +30,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -77,6 +78,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
     
+    def _update_aliens(self):
+        """Verifica se a frota está na borda e, em seguida, atualiza as posições"""
+        self._check_fleet_edges()
+        self.aliens.update()
+    
     def _create_fleet(self):
         """Cria a frota de alienígenas"""
         # Cria um alienígena e continua adicionando alienígenas até que não haja mais espaço
@@ -101,8 +107,20 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
-        
     
+    def _check_fleet_edges(self):
+        """"Responde apropriadamente se algum alienígena alcançou uma borda"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Faz toda a frota descer e mudar de direção"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         """Atualiza as imagens na tela e muda para a nova tela"""
         self.screen.fill(self.settings.bg_color)
