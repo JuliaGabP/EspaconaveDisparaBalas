b937d66a4802987adc0f67cb68c4cc97530ffb5e
Julia <juliagabpaulino@outlook.com>
2026-03-02 13:35:36 -0300

    Adicionando quinta parte do desenvolvimento: Criando o primeiro alienígena

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Classe geral para gerenciar ativos e comportamento do jogo"""
@@ -19,6 +20,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+        self._create_fleet()
         
     def run_game(self):
         """Inicia o loop principal do jogo"""
@@ -72,7 +76,13 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-                
+    
+    def _create_fleet(self):
+        """Cria a frota de alienígenas"""
+        # Cria um alienígena
+        alien = Alien(self)
+        self.aliens.add(alien)
+        
     
     def _update_screen(self):
         """Atualiza as imagens na tela e muda para a nova tela"""
@@ -80,6 +90,7 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
 
